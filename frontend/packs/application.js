e28837a2dcfed252b6b88c82e88d3c92bf698555
Georgiy <g3orge.miller@gmail.com>
2019-03-16 16:30:50 +0300

    testing JS linting

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -8,7 +8,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,7 +15,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import './application.css';
+import "./application.css";
 
-document.body.insertAdjacentHTML('afterbegin', 'Webpacker really works!')
-
+document.body.insertAdjacentHTML("afterbegin", "Webpacker really works!");
